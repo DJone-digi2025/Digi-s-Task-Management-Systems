2ed268034af6b9dd45f3f8990d1ee029fc8e8139
DJone-digi2025 <davidjones.digi.2025@gmail.com>
2026-03-12 00:46:48 +0530

    fix google auth for render

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json",
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 })
 
